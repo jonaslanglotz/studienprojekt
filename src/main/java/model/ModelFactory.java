f1ba61190fcbbcda7da33829ccb672f61305d14b
jonaslanglotz <jonas.l1512@gmail.com>
2021-06-02 15:24:00 +0200

    Updated the ModelFactory so it creates a world model.

--- src/main/java/model/ModelFactory.java
+++ src/main/java/model/ModelFactory.java
@@ -1,4 +1,13 @@
 package main.java.model;
 
+import lombok.Getter;
+
 public class ModelFactory {
+
+    @Getter
+    WorldModel worldModel;
+
+    public ModelFactory ()  {
+        this.worldModel = new DefaultWorldModel(100, 100, 1);
+    }
 }
